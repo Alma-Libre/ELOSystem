1cf606c3da6513bcaa60169bf79dcd250e9149ea
Mayank Gautam <mayankgautam98@gmail.com>
2019-02-04 19:37:10 +0530

    react router and login form

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
 import App from './App';
+import Login from './login';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <div>
+            <aside>
+                Hi, there!
+                <Link to={`/`}>App</Link>
+                <Link to={`/login`}>Login</Link>
+            </aside>
+            <main>
+                <Route exact path="/" component={App} />
+                <Route path="/login" component={Login} />
+            </main>
+        </div>
+    </Router>,
+    document.getElementById('root')
+);
---
+++ src/client/login.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+// import axios from 'axios';
+
+export default class Login extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {username: '',password:'',redirectTo:''};
+
+		this.handleUsername = this.handleUsername.bind(this);
+		this.handlePassword = this.handlePassword.bind(this);
+
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleUsername(event) {
+		this.setState({username: event.target.value});
+	}
+	handlePassword(event) {
+		this.setState({password: event.target.value});
+	}
+
+	handleSubmit(event) {
+		//alert('Username is ' + this.state.username + ' Password is' + this.state.password);
+		event.preventDefault();
+		// axios.post('/authenticate',{
+		// 	username: this.state.username,
+		// 	password: this.state.password 
+		// })
+		// .then(res => {
+		// 	if(res.data){
+		// 		console.log("user-signed-in");
+		// 		this.setState({redirectTo:'/login'});
+		// 	}
+		// 	else{
+		// 		console.log("Sign-In Error");
+		// 	}
+		// }).catch(err => {
+		// 		console.log("Server-Side Error");
+		// })
+	}
+
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<label>
+					Username:
+					<input type="text" value={this.state.username} onChange={this.handleUsername} />
+				</label>
+				<br/>
+				<br/>
+				<label>
+					Password:
+					<input type="text" value={this.state.password} onChange={this.handlePassword} />
+				</label>
+				<br/>
+				<br/>
+				<input type="submit" value="Submit" />
+			</form>
+		);
+	}
+}
